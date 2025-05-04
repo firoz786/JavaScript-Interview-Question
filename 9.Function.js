13ab9fa4618d6f5d35e81ee8c6e481b696f86f8b
firoz alam <firozalam786123@gmail.com>
2025-05-04 07:20:48 +0000

    function , iife

---
+++ 9.Function.js
@@ -0,0 +1,25 @@
+function Function() {
+    //Example :  Function Hoisting
+    console.log(greet("Alice")); // we are calling the function here before defining it. Hoisting. 
+    function greet(name) {
+        return `Hello, ${name}!`;
+    }
+
+
+    //Example :  Function expression me hoisting nahi hota
+    // console.log(greet1("Bob")); // Output: Hello, Bob! // TypeError: greet is not a function --> idhar hositing nahi hota
+    const greet1 = function (name) {
+        return `Hello, ${name}!`;
+    };
+    console.log(greet1("Bob")); // Output: Hello, Bob! 
+
+
+    //Example :  Rest and Spread operator
+    function add(...numbers) {
+        return numbers.reduce((acc, num) => acc + num * 2, 2);
+    }
+    console.log(add(1, 2));
+
+    (()=>{console.log("This is called IIFE imediate invk fun exp")})();
+}
+Function();
